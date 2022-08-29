ce33ab0af99936fa7071da676528cd65a41ea46f
Dean99712 <Dean2910997@gmail.com>
2022-08-29 19:39:25 +0300

    Changed base Url configuration

--- src/main/java/com/example/RestApiProject/config/WebClientConfiguration.java
+++ src/main/java/com/example/RestApiProject/config/WebClientConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebClientConfiguration {
+public class    WebClientConfiguration {
 
     @Autowired
     private RestConfiguration configuration;
